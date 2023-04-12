3304df1919c257e3b96d56eba3845a8b2dbfbad3
Malay Patra <107231583+malay77patra@users.noreply.github.com>
2023-04-12 10:05:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,40 +6,60 @@ from splitall import extract_code
 import json
 
 with open('config.json', 'r') as f:
-    config = json.load(f)
-API_KEY = config["API_KEY"]
-i = 0
-v = 0
-work = "work" + str(v)
-file_list = {}
+  config = json.load(f)
 
+API_KEY = config["API_KEY"]
+username = config["username"]
+password = config["pat"]
+#
+i = 0
+file_list = {}
+api_settings = {
+  "engine": "text-davinci-003",
+  "max_tokens": 2048,
+  "n": 1,
+  "stop": None,
+  "temperature": 0.3,
+}
+
+URL = input("Enter the repo url: ")
+base_name = os.path.basename(URL).rstrip(".git")
+work = base_name + str(i)
 while True:
   if os.path.exists(os.path.join(work)):
     i += 1
-    work = "work" + str(i)
+    work = base_name + str(i)
   else:
     break
-
-URL = input("URL: ")
-#   https://github.com/malay9418/PYTHON_PROJECTS
+docs = os.path.join(work, "docs")
 
 #clone
 
 repo_url = URL
 print("Cloning repo...")
-repo_name = os.path.join(work, os.path.basename(repo_url)).rstrip(".git")
-Repo.clone_from(repo_url, repo_name)
-print("Cloned succesfully")
-docs = os.path.basename(repo_url).rstrip(".git") + "-docs" + str(i)
+repo_name = work
+try:
+  Repo.clone_from(repo_url, repo_name)
+except:
+  try:
+    ending = URL.replace("https://", "")
+    starting = f"https://{username}:{password}@"
+    repo_url = starting + ending
+    Repo.clone_from(repo_url, repo_name)
+  except Exception as e:
+    print(e)
+
 
 repo_dir = os.path.abspath(repo_name)
 root_path = os.path.join(docs)
-dest_path = os.path.join(work)
 here = os.path.abspath(os.path.join(os.path.dirname(__file__), work))
 
-#print(repo_dir, docs_dir)
+print("Creating needed files and dirs...")
+os.mkdir(docs)
 
-print("creating needed files and dirs...")
+
+def cleare_path(patho):
+  shutil.rmtree(os.path.join(patho))
 
 
 def make_tree(root_dir):
@@ -55,10 +75,8 @@ def make_tree(root_dir):
     #
     if os.path.isfile(item_path):
       if item.endswith(".py"):
-        #print("file: ", item)
         path_data.update({item: "file"})
     elif os.path.isdir(item_path):
-      #print("dir: ", item)
       data = make_tree(item_path)
       path_data.update({item: data})
   return path_data
@@ -69,45 +87,64 @@ trees = make_tree(here)
 print("Analyzing file system...")
 
 
-def create_file_tree(tree, root_path, dest_path):
+def create_file_tree(tree, root_path):
+  md_path = root_path.replace("/docs", "")
+  parent_files = os.listdir(md_path)
+  py_files = [file for file in parent_files if file.endswith('.py')]
+  if len(py_files) != 0:
+    with open(os.path.join(md_path, "README.md"), "a") as g:
+      g.write("***\n# Read The Documentation For Better Understanding\n")
+      for file in py_files:
+        md_path_new = os.path.join(root_path, file).replace(".py", ".md")
+        rel_md_path = os.path.relpath(md_path_new, md_path)
+        md = f"\n📃 [{file}]({file}) - [Documentation]({rel_md_path})\n"
+        g.write(md)
+
   global file_list
   for node, content in tree.items():
+    if node == 'docs' or node == 'venv':
+      continue
     node_path = os.path.join(root_path, node)
     if isinstance(content, dict):
       os.makedirs(node_path, exist_ok=True)
-      create_file_tree(content, node_path, dest_path)
+      create_file_tree(content, node_path)
     else:
       init_path = work + node_path.split(docs)[1]
       node_path = os.path.splitext(node_path)[0] + '.md'
       file_list.update({init_path: node_path})
-      with open(node_path, 'w') as f:
-        f.write("")
+      os.mknod(node_path)
 
 
-create_file_tree(trees, root_path, dest_path)
-#print(file_list)
-print(f"total {len(file_list)} python scripts found")
+create_file_tree(trees, root_path)
+
+print(f"Total number of python scripts found: {len(file_list)}")
+
 
 def write_into_file():
-  print("Generating documentation...")
+  print(
+    "Analyzing codes and Generating documentation...\nPlease wait it may take a few minutes to complete!"
+  )
   for parent in file_list:
     child = file_list[parent]
     with open(child, "a") as f:
+      print(f"Analyzing file: {parent}")
       data = extract_code(parent)
       for typos in data:
-        if (len(data[typos])!=0):
+        if (len(data[typos]) != 0):
           f.write("\n***\n# " + typos + "\n***\n")
           elems = data[typos]
-          response = explain_code(API_KEY, typos, elems)
+          response = explain_code(API_KEY, api_settings, typos, elems)
           f.write(response)
-    print(parent, "succesfully documented")
+    print(parent, "--succesfully documented ✔️")
+
 
 write_into_file()
 
+print(f"\nAll the documentation are saved into {docs} directory ✔️")
 
-print(f"All the dpcumentation are saved into {docs} directory")
+#please don't uncomment below line , It will remove all the generated docs, this is just for testing purpose root_path.replace("/docs", "")
+"""
 def cleare_path(path):
   shutil.rmtree(path)
-
-
-cleare_path(os.path.join(work))
+#cleare_path(os.path.join(work))
+"""
